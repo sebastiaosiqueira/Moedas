c48804de7d9d2b6650079243bb39bebe55ad9e47
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2024-12-13 11:01:04 -0400

    versao final

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+namespace  Moedas
+{
+    class Program{
+        static void Main(string[] args){
+            Console.Clear();
+
+            decimal valor =10.25m;
+           
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-BR"))
+                ); 
+               decimal valor2 = 10536.25m;
+
+                Math.Round(valor2);
+                Console.WriteLine(valor2);
+                Math.Ceiling(valor2);
+                 Console.WriteLine(valor2);
+                 Math.Floor(valor2);
+                 Console.WriteLine(valor2);
+
+
+         }
+    }
+    
+}
